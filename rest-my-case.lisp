7a299ef3b72e3f3883787a3599fd6288ccbdbc4f
ch <ch@mr-co.de>
2008-05-14 22:43:24 +0200

    Encoding the url is done by jquery.
    darcs-hash:20080514204324-c72ca-ca2e2d7754d831014aab7107883d1a488a59829a.gz

--- rest-my-case.lisp
+++ rest-my-case.lisp
@@ -105,7 +105,7 @@
     (if (null url-args)
 	url*
 	`(+ ,url*
-	    ,@(mapcan (lambda (x) (list "/" `(encode-u-r-i ,(cdr x))))
+	    ,@(mapcan (lambda (x) (list "/" (cdr x)))
 		      (fill-holes (sort url-args #'< :key #'car)
 				  "x"))))))
 
